d903695a4171050a6b53a16da987f56077dfd875
Silicon50 <ndukwechukwuaja@gmail.com>
2023-05-23 16:56:20 +0100

    string functions

--- demo.c
+++ demo.c
@@ -2,13 +2,14 @@
 char* cf_strdup(char *strings);
 char* cf_strdup(char *strings);
 int cf_strcmp(char *first_str, char *second_str);
+char *cf_strcat(char *first_str, char *second_str);
 
 int main()
 {
 char str1[] = "Run";
 char str2[] = "Runto";
-int str3;
-str3 = cf_strcmp(str1,str2);
-printf("%d\n", str3);
+char *str3;
+str3 = cf_strcat(str1,str2);
+printf("%s\n", str3);
 return (0);
 }
